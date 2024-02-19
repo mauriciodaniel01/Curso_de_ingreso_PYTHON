5820067099f7534f7bf370546892a0646db9d084
Mauricio Bottaro <bottaromauriciodaniel@gmail.com>
2024-02-19 12:14:19 -0300

    ejercicio 4 ok

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_04.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_04.py
@@ -33,7 +33,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre = prompt ("Nombre" , "Igresar nombre")
+        self.txt_nombre.delete(0, "end")
+        self.txt_nombre.insert(0, nombre)
         
     
 if __name__ == "__main__":
