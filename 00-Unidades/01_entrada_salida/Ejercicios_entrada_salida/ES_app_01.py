6f2fd7ed8d3a88d1ff99e9da91de5d83b389c9ed
Mauricio Bottaro <bottaromauriciodaniel@gmail.com>
2024-02-19 10:03:20 -0300

    Ejercicio 2 ok

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
@@ -5,8 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Mauricio
+apellido: Bottaro
+tutor: Esteban
 ---
 Ejercicio: entrada_salida_01
 ---
--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -27,7 +27,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        descripcion_producto  = prompt( "Buscador" , "Producto deaseado")
+        descripcion_producto = "Pepsi"
+        alert ("Descripcion", descripcion_producto)
         
         
     
